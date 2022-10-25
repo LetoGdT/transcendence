6328aa4c283e2ac675ef35f2294d767c9334191d
lucille <lucille.burnet@gmail.com>
2022-10-25 14:54:15 +0200

    humm

--- front-end/ft_transcendence/src/menu-zone.tsx
+++ front-end/ft_transcendence/src/menu-zone.tsx
@@ -3,28 +3,47 @@ import { useState } from 'react'
 import './App.css'
 
 class Menu extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			item_menu: "",
+		};
+	}
+
+	handleClick(item_menu) {
+		if (item_menu = "home")
+	}
+
 	render() {
 		return(
 			<nav className='Menu'>
 				<table>
 					<tr>
 						<td>
-							<button>
+							<button /*onClick={() => }*/>
 								<img src="src/home.png" className="menu_button" alt="Home" />
 							</button>
 						</td>
 						<td>
-							<button onClick={() => /*je sais pas*/>
+							<button onClick={() => 
+								<tr>spec a match</tr>
+								<tr>play</tr>
+							}>
 								<img src="src/pong.png" className="menu_button" alt="Pong" />
 							</button>
 						</td>
 						<td>
-							<button>
+							<button /*onClick={() => }*/>
 								<img src="src/chat.png" className="menu_button" alt="Chat" />
 							</button>
 						</td>
 						<td>
-							<button>
+							<button onClick={() => 
+								<tr>Settings</tr>
+								<tr>Stats</tr>
+								<tr>Friend list</tr>
+								<tr>Matchs history</tr>
+							}>
 								<img src="src/profile.png" className="menu_button" alt="Profile" />
 							</button>
 						</td>
