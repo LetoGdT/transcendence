93020692168969cb8d8c6b1ce95b732aeb8acb6c
lucille <lucille.burnet@gmail.com>
2022-10-28 16:10:22 +0200

    working on menu

--- front-end/ft_transcendence/src/menu-zone.tsx
+++ front-end/ft_transcendence/src/menu-zone.tsx
@@ -5,84 +5,134 @@ import profileIcon from './assets/user-solid.svg'
 import chatIcon from './assets/comment-solid.svg'
 import { useState } from 'react'
 import './App.css'
-
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon";
+import { SvgIcon } from '@mui/material';
 
-export default function OurMenu() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+export default function BasicMenu() {
+  const [anchorElHome, setAnchorElHome] = React.useState<null | HTMLElement>(null);
+  const [anchorElPong, setAnchorElPong] = React.useState<null | HTMLElement>(null);
+  const [anchorElChat, setAnchorElChat] = React.useState<null | HTMLElement>(null);
+  const [anchorElProfile, setAnchorElProfile] = React.useState<null | HTMLElement>(null);
+
+  const openHome = Boolean(anchorElHome);
+  const openPong = Boolean(anchorElPong);
+  const openChat = Boolean(anchorElChat);
+  const openProfile = Boolean(anchorElProfile);
+  const handleClickHome = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElHome(event.currentTarget);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleCloseHome = () => {
+    setAnchorElHome(null);
   };
+  const handleClickPong = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElPong(event.currentTarget);
+  };
+  const handleClosePong = () => {
+    setAnchorElPong(null);
+  };
+  const handleClickChat = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElChat(event.currentTarget);
+  };
+  const handleCloseChat = () => {
+    setAnchorElChat(null);
+  };
+  const handleClickProfile = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElProfile(event.currentTarget);
+  };
+  const handleCloseProfile = () => {
+    setAnchorElProfile(null);
+  };
+
   return (
-    <React.Fragment>
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-        <Tooltip title="Pong Game">
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2 }}
-            aria-controls={open ? 'pong-gama' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-          >
-            <SvgIcon>
-				<path d="M416 288c-50.1 0-93.6 28.8-114.6 70.8L68.9 126.3l.6-.6 60.1-60.1c87.5-87.5 229.3-87.5 316.8 0c67.1 67.1 82.7 166.3 46.8 248.3C471.8 297.6 445 288 416 288zM49.3 151.9L290.1 392.7c-1.4 7.5-2.1 15.3-2.1 23.3c0 23.2 6.2 44.9 16.9 63.7c-3 .2-6.1 .3-9.2 .3H293c-33.9 0-66.5-13.5-90.5-37.5l-9.8-9.8c-13.1-13.1-34.6-12.4-46.8 1.7L88.2 501c-5.8 6.7-14.2 10.7-23 11s-17.5-3.1-23.8-9.4l-32-32C3.1 464.3-.3 455.7 0 446.9s4.3-17.2 11-23l66.6-57.7c14-12.2 14.8-33.7 1.7-46.8l-9.8-9.8C45.5 285.5 32 252.9 32 219v-2.7c0-22.8 6.1-44.9 17.3-64.3zM416 512c-53 0-96-43-96-96s43-96 96-96s96 43 96 96s-43 96-96 96z"/>
-			</SvgIcon>
-          </IconButton>
-        </Tooltip>
-      </Box>
-      <Menu
-        anchorEl={anchorEl}
-        id="pong-game"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1.5,
-            '& .MuiAvatar-root': {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            '&:before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={openHome ? 'home-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={openHome ? 'true' : undefined}
+        onClick={handleClickHome}
       >
-        <MenuItem>
-          Spec a match
-        </MenuItem>
-        <MenuItem>
-          Play
-        </MenuItem>
+        Home
+      </Button>
+      <Menu
+        id="home-menu"
+        anchorEl={anchorElHome}
+        open={openHome}
+        onClose={handleCloseHome}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleCloseHome}>Nothing in home</MenuItem>
       </Menu>
-    </React.Fragment>
+      <Button
+        id="basic-button"
+        aria-controls={openPong ? 'pong-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={openPong  ? 'true' : undefined}
+        onClick={handleClickPong}
+      >
+        Pong
+      </Button>
+      <Menu
+        id="pong-menu"
+        anchorEl={anchorElPong}
+        open={openPong}
+        onClose={handleClosePong}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClosePong}>Spec a match</MenuItem>
+        <MenuItem onClick={handleClosePong}>Play</MenuItem>
+      </Menu>
+      <Button
+        id="basic-button"
+        aria-controls={openChat ? 'chat-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={openChat ? 'true' : undefined}
+        onClick={handleClickChat}
+      >
+        Chat
+      </Button>
+      <Menu
+        id="chat-menu"
+        anchorEl={anchorElChat}
+        open={openChat}
+        onClose={handleCloseChat}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleCloseChat}>Nothiing in Chat</MenuItem>
+      </Menu>
+      <Button
+        id="basic-button"
+        aria-controls={openProfile ? 'profile-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={openProfile ? 'true' : undefined}
+        onClick={handleClickProfile}
+      >
+        Profile
+      </Button>
+      <Menu
+        id="profile-menu"
+        anchorEl={anchorElProfile}
+        open={openProfile}
+        onClose={handleCloseProfile}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleCloseProfile}>Friends</MenuItem>
+        <MenuItem onClick={handleCloseProfile}>Stats</MenuItem>
+        <MenuItem onClick={handleCloseProfile}>Match history</MenuItem>
+        <MenuItem onClick={handleCloseProfile}>Settings</MenuItem>
+      </Menu>
+    </div>
   );
 }
+
