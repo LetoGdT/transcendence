d38e720e1884fcbf47c6af776d17192d2f96632f
lucille <lucille.burnet@gmail.com>
2022-10-31 11:49:54 +0100

    working on menu

--- front-end/ft_transcendence/website/src/App.tsx
+++ front-end/ft_transcendence/website/src/App.tsx
@@ -5,7 +5,7 @@ import { OurMenu } from './menu-zone';
 import { Home } from './adaptable-zone';
 import { Chat } from './adaptable-zone';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 
 function App() {
@@ -21,7 +21,14 @@ function App() {
 				<OurMenu/>
 			</div>
 			<div className='Adaptable'>
-
+				<Routes>
+					<Route path="/home">
+						<Home/>
+					</Route>
+					<Route path="/chat">
+							<Chat/>
+					</Route>
+				</Routes>
 			</div>
 		</Router>
 	</React.Fragment>
