d41a16c9d210cbbea8c58ebad79d0e08f7103cbf
lucille <lucille.burnet@gmail.com>
2022-10-31 11:23:55 +0100

    working on menu

--- front-end/ft_transcendence/website/src/App.tsx
+++ front-end/ft_transcendence/website/src/App.tsx
@@ -2,17 +2,28 @@ import React from 'react';
 import './App.css';
 import { OurMenu } from './menu-zone';
 
+import { Home } from './adaptable-zone';
+import { Chat } from './adaptable-zone';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+
 function App() {
   return (
 	<React.Fragment>
-		<div>
-			<header className="Header">
-				
-			</header>
-		</div>
-		<div className='Menu'>
-			<OurMenu/>
-		</div>
+		<Router>
+			<div>
+				<header className="Header">
+					baniere + avatar-zone
+				</header>
+			</div>
+			<div className='Menu'>
+				<OurMenu/>
+			</div>
+			<div className='Adaptable'>
+
+			</div>
+		</Router>
 	</React.Fragment>
 	
   );
---
+++ front-end/ft_transcendence/website/src/adaptable-zone.tsx
@@ -0,0 +1,67 @@
+import './App.css'
+
+import * as React from 'react';
+
+export function Chat(){
+	return(
+		<div>
+			<h1>Chat</h1>
+		</div>
+	);
+}
+
+export function Friends(){
+	return(
+		<div>
+			Friends
+		</div>
+	);
+}
+
+export function Home(){
+	return(
+		<div>
+			<h1>Home</h1>
+		</div>
+	);
+}
+
+export function MatchHistory(){
+	return(
+		<div>
+			MatchHistory
+		</div>
+	);
+}
+
+export function Play(){
+	return(
+		<div>
+			Play
+		</div>
+	);
+}
+
+export function Profile(){
+	return(
+		<div>
+			Profile
+		</div>
+	);
+}
+
+export function SpecAMatch(){
+	return(
+		<div>
+			SpecAMatch
+		</div>
+	);
+}
+
+export function Stats(){
+	return(
+		<div>
+			Stats
+		</div>
+	);
+}
