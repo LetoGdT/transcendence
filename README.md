2e8c4f9e8ae20284f10d7deee27dec7c88afb266
lucille <lucille.burnet@gmail.com>
2022-11-02 13:55:48 +0100

    working on menu

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 <ul>pour le Front-End
 	<li>vite (webpacker): https://vitejs.dev/guide/</li>
 	<li>npm react-dom: https://www.npmjs.com/package/react-dom</li>
-	<li>Dropdown Menu tuto: https://blog.logrocket.com/how-create-multilevel-dropdown-menu-react/</li>
+	<li>how to use route https://openclassrooms.com/en/courses/7315991-intermediate-react/7572037-turn-your-application-into-a-single-page-app-with-react-router</li>
+	<li>make route work as v6 https://stackoverflow.com/questions/70074873/reactjs-home-is-not-a-route-component-all-component-children-of-routes-m</li>
+	<li>@mui https://mui.com/</li>
 </ul>
--- front-end/ft_transcendence/website/src/App.tsx
+++ front-end/ft_transcendence/website/src/App.tsx
@@ -3,7 +3,9 @@ import './App.css';
 import { OurMenu } from './menu-zone';
 
 import { Home } from './adaptable-zone';
+import { Play, SpecAMatch} from './adaptable-zone';
 import { Chat } from './adaptable-zone';
+import { Friends, MatchHistory, Profile, Stats } from './adaptable-zone';
 
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
@@ -21,16 +23,17 @@ function App() {
 				<OurMenu/>
 			</div>
 			<div className='Adaptable'>
-				{/**  
+
 				<Routes>
-					<Route path="/home">
-						<Home/>
-					</Route>
-					<Route path="/chat">
-							<Chat/>
-					</Route>
+					<Route path="/chat" element={<Chat/>} />
+					<Route path="/friends" element={<Friends/>} />
+					<Route path="/" element={<Home />} />
+					<Route path="/matchhistory" element={<MatchHistory/>} />
+					<Route path="/play" element={<Play/>} />
+					<Route path="/profile" element={<Profile/>} />
+					<Route path="/specamatch" element={<SpecAMatch/>} />
+					<Route path="/stats" element={<Stats/>} />
 				</Routes>
-				*/}
 			</div>
 		</Router>
 	</React.Fragment>
