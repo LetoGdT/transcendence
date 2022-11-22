211dcecfe1601bc1a0955583a5350649fa9d6898
lucille <lucille.burnet@gmail.com>
2022-11-22 15:24:23 +0100

    working on Settings : some things do not display yet

--- front-end/ft_transcendence/website/src/App.tsx
+++ front-end/ft_transcendence/website/src/App.tsx
@@ -5,7 +5,7 @@ import { OurMenu } from './menu-zone';
 import { Home } from './adaptable-zone';
 import { Play, SpecAMatch} from './adaptable-zone';
 import { Chat } from './adaptable-zone';
-import { Friends, MatchHistory, Profile, Stats } from './adaptable-zone';
+import { Friends, MatchHistory, Settings, Profile } from './adaptable-zone';
 import { PleaseConnect, SignOn } from './adaptable-zone';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
@@ -30,9 +30,9 @@ function App() {
 						<Route path="/" element={<Home />} />
 						<Route path="/matchhistory" element={<MatchHistory/>} />
 						<Route path="/play" element={<Play/>} />
-						<Route path="/profile" element={<Profile/>} />
+						<Route path="/settings" element={<Settings/>} />
 						<Route path="/specamatch" element={<SpecAMatch/>} />
-						<Route path="/stats" element={<Stats/>} />
+						<Route path="/profile" element={<Profile/>} />
 						<Route path="/pleaseconnect" element={<PleaseConnect/>} />
 						<Route path="/signon" element={<SignOn/>} />
 					</Routes>
