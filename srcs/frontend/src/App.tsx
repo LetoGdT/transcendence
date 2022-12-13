31db1889e22b1babe624bb792980651e211c1954
lucille <lucille.burnet@gmail.com>
2022-12-13 16:32:25 +0100

    obsession du pixel

--- srcs/frontend/src/App.tsx
+++ srcs/frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { OurMenu } from './menu-zone';
 import { Home } from './adaptable-zone';
 import { Play, SpecAMatch} from './adaptable-zone';
 import { Chat } from './chat-zone';
-import { Friends, MatchHistory, Settings, Profile } from './adaptable-zone';
+import { Friends, MatchHistory, Settings, Profile, OtherProfile } from './adaptable-zone';
 import { PleaseConnect, SignOn } from './adaptable-zone';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
@@ -32,6 +32,7 @@ function App() {
 						<Route path="/play" element={<Play/>} />
 						<Route path="/settings" element={<Settings/>} />
 						<Route path="/specamatch" element={<SpecAMatch/>} />
+						{/* <Route path="/otherprofile" element={<OtherProfile/>} /> */}
 						<Route path="/profile" element={<Profile/>} />
 						<Route path="/pleaseconnect" element={<PleaseConnect/>} />
 						<Route path="/signon" element={<SignOn/>} />
--- srcs/frontend/src/adaptable-zone.tsx
+++ srcs/frontend/src/adaptable-zone.tsx
@@ -3,10 +3,9 @@ import './App.css'
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-
 import { Link } from 'react-router-dom';
 
-import Avatar from './link_botw_avatar.jpg';
+import Avatar from './link_botw_avatar.jpg';//a enlever quand plus nec
 
 import OffLine from './offline.png';
 import OnLine from './online.png';
@@ -16,9 +15,7 @@ import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
-
 import { Chart } from "react-google-charts";
-
 import { useState, useEffect } from "react";
 
 type resultProps = {
@@ -502,6 +499,78 @@ export const gameData = [
 	["Defeats", 5],
 	["Draws", 2],
 ];
+
+export function OtherProfile(pseudo: string){
+	const [data, setResult] = useState<resultProps>();
+
+	useEffect(() => {
+		const api = async () => {
+		  const data = await fetch("http://localhost:9999/api/users/" + pseudo, {
+			method: "GET",
+			credentials: 'include'
+		  });
+		  const jsonData = await data.json();
+		  setResult(jsonData);
+		  console.log(jsonData);
+		};
+	
+		api();
+	  }, []);
+	return(
+		<React.Fragment>
+			<h1>Profile - Stats</h1>
+			<p>si user non connecter renvoyer vers /pleaseconnect</p>
+			<div className='Profile-container'>
+				<div className='Profile-Alias'>
+					<div className='Profile-Alias-div'>{data?.username}</div>
+					{/* <div>{data?.email}</div> */}
+					<div className='Profile-Alias-div'><AddButton variant="contained" disableRipple>Add to Friends</AddButton></div>
+					{/* <div className='Profile-Alias-div'><RemoveButton variant="contained" disableRipple>Remove from Friends</RemoveButton></div> */}
+				</div>
+				<div className='Profile-container-row-lvl1'>
+					<div className='Profile-Avatar'>
+						<img src={data?.image_url} alt="alias' avatar" className='Settings-avatar-img'></img>
+					</div>
+					<div className='Profile-Pie-Charts'>
+						<Chart
+							chartType="PieChart"
+							data={gameData}
+							options={options}
+							width={"100%"}
+							height={"400"}
+						/>
+					</div>
+				</div>
+				<div>
+					<div className='Profile-game-info'>
+						<div><b>Rank:</b> {data?.rank}</div>
+						<div><b>Level:</b> {data?.level}</div>
+					</div>
+					<h4>Achievements</h4>
+					<div className='Profile-achievement-container'>
+						<div className='Profile-achievement-container-div'>
+							<div>
+								<img src={Avatar} alt='achievement 1' className='Profile-achievement-container-div-img'></img>
+							</div>
+							<div>
+								Achivement 1
+							</div>
+						</div>
+						<div className='Profile-achievement-container-div'>
+							<div>
+								<img src={Avatar} alt='achievement 2' className='Profile-achievement-container-div-img'></img>
+							</div>
+							<div>
+								Achivement 2
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</React.Fragment>
+	);
+}
 
 export function Profile(){
 	const [data, setResult] = useState<resultProps>();
