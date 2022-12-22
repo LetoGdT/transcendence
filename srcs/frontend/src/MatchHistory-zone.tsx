ee26a88e1b8df22e9752e7100738b0fca576d2f6
lucille <lucille.burnet@gmail.com>
2022-12-22 18:47:44 +0100

    update struct

--- srcs/frontend/src/MatchHistory-zone.tsx
+++ srcs/frontend/src/MatchHistory-zone.tsx
@@ -8,29 +8,29 @@ import { useState, useEffect } from "react";
 import { PleaseConnect } from './adaptable-zone';
 
 type resultProps = {
-	email: string;
-	username: string;
-	image_url: string;
-	status: string;
-	rank: number;
-	level: number;
-	achievement: [];//?
-	//map avec par exemple id = nom de l'achievement, value = url d'une image
-	winNb: number;
-	loseNb: number;
-	friends: [];//?
-	//une map pour ses friends (key = id du friend, value = structure similaire du friend)
-	matchHistory: [];//?
-	/*
-		il me faudrait une liste avec :
-			score du player
-			pseudo de l'adversaire
-			si l'adversaire est un friend
-			score de l'adversaire
-		une autre (???) avec :
-			niveau fait ?
-			reussite ? score ?
-		*/
+	// email: string;
+	// username: string;
+	// image_url: string;
+	// status: string;
+	// rank: number;
+	// level: number;
+	// achievement: [];//?
+	// //map avec par exemple id = nom de l'achievement, value = url d'une image
+	// winNb: number;
+	// loseNb: number;
+	// friends: [];//?
+	// //une map pour ses friends (key = id du friend, value = structure similaire du friend)
+	// matchHistory: [];//?
+	// /*
+	// 	il me faudrait une liste avec :
+	// 		score du player
+	// 		pseudo de l'adversaire
+	// 		si l'adversaire est un friend
+	// 		score de l'adversaire
+	// 	une autre (???) avec :
+	// 		niveau fait ?
+	// 		reussite ? score ?
+	// 	*/
 };
 
 export function MatchHistory(){
