f5c441cc9d4c72e9f8bc6214bf047e884643f37e
lucille <lucille.burnet@gmail.com>
2022-12-22 17:20:53 +0100

    ENFIN j'ai otherprofile qui s'affiche comme je veux

--- srcs/frontend/src/App.tsx
+++ srcs/frontend/src/App.tsx
@@ -17,29 +17,124 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom'
 
+// type resultProps = {
+// 	data: [];
+// }
+
+// export function ListUser(){//vouer à disparaitre
+// 	const [data, setResult] = useState<resultProps>();
+	
+// 	useEffect(() => {
+// 		const api = async () => {
+// 			let urltofetch : string;
+// 			urltofetch = `http://localhost:9999/api/users/`;
+// 			const data = await fetch(urltofetch, {
+// 				method: "GET",
+// 				credentials: 'include'
+// 			});
+// 			const jsonData = await data.json();
+// 			setResult(jsonData);
+// 		};
+	
+// 		api();
+// 	}, []);
+// 	return(
+// 		<div>
+// 			{data?.data.map((user: any) => {
+// 				var url: string = "/otherprofile";
+// 				url = url.concat("/");
+// 				url = url.concat(user.id);
+// 				return(
+
+// 					<div>
+// 						<Link to={url} >
+// 							{user.uid}
+// 						</Link>
+// 					</div>
+// 				);
+// 			})}
+// 		</div>
+// 	);
+// }
+
 type resultProps = {
 	data: [];
+	meta : {
+		page: number;
+		take: number;
+		itemCount: number;
+		pageCount: number;
+		hasPreviousPage: boolean;
+		hasNextPage: boolean;
+	};
 }
 
 export function ListUser(){//vouer à disparaitre
 	const [data, setResult] = useState<resultProps>();
 	
+	let users: any[];
+	let pagecount: number | undefined;
+	
 	useEffect(() => {
 		const api = async () => {
 			let urltofetch : string;
-			urltofetch = `http://localhost:9999/api/users/`;
-			const data = await fetch(urltofetch, {
+			urltofetch = `http://localhost:9999/api/users`;
+			var data = await fetch(urltofetch, {
 				method: "GET",
 				credentials: 'include'
 			});
-			const jsonData = await data.json();
+			var jsonData = await data.json();
 			setResult(jsonData);
+
+			// console.log(jsonData.meta);
+			pagecount = data?.meta.pageCount;
+			// for (let i = 1; i < pagecount; i++){
+				urltofetch = `http://localhost:9999/api/users/?`;
+				var data = await fetch(urltofetch 
+					+ new URLSearchParams({
+					page: '1',//i.toString(), 
+				})
+					, {
+					method: "GET",
+					credentials: 'include'
+				});
+				var jsonData = await data.json();
+				setResult(jsonData);
+				users = users.concat(data?.data);
+			// }
 		};
 	
 		api();
 	}, []);
+	
+
+	// for (let i = 1; i < pagecount; i++){
+	// 	useEffect(() => {
+	// 		const api = async () => {
+	// 			let urltofetch : string;
+	// 			urltofetch = `http://localhost:9999/api/users/?`;
+	// 			const data = await fetch(urltofetch 
+	// 				+ new URLSearchParams({
+	// 				page: i.toString(), 
+	// 			})
+	// 				, {
+	// 				method: "GET",
+	// 				credentials: 'include'
+	// 			});
+	// 			const jsonData = await data.json();
+	// 			setResult(jsonData);
+	// 		};
+		
+	// 		api();
+	// 	}, []);
+	// 	users = users.concat(data?.data);
+	// }
+	
 	return(
 		<div>
+			{/* <div>
+				{pagecount}
+			</div> */}
 			{data?.data.map((user: any) => {
 				var url: string = "/otherprofile";
 				url = url.concat("/");
@@ -48,7 +143,7 @@ export function ListUser(){//vouer à disparaitre
 
 					<div>
 						<Link to={url} >
-							{user.uid}
+							{user.username}
 						</Link>
 					</div>
 				);
