60efe9add16c8b2873b2f291487240a1aeca25a4
Sylvain Ducamp <sylducam@gmail.com>
2022-12-23 18:12:22 +0100

    the ball is moving

--- srcs/frontend/src/pong_the_one/pong.js
+++ srcs/frontend/src/pong_the_one/pong.js
@@ -6,8 +6,9 @@ let game; // del need type
 const PLAYER_HEIGHT = 100;
 const PLAYER_WIDTH = 5;
 
-function draw() {
-    var ctx = canvas.getContext('2d');
+function draw()
+{
+    let ctx = canvas.getContext('2d');
 	
 	// Draw field
 	ctx.fillStyle = 'black';
@@ -22,17 +23,23 @@ function draw() {
     ctx.stroke();
 	// Draw players
 	ctx.fillStyle = 'white';
-	ctx.fillRect(0, game.player.y, PLAYER_WIDTH, PLAYER_HEIGHT);
-	ctx.fillRect(canvas.width - PLAYER_WIDTH, game.computer.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+	ctx.fillRect(0, game.player1.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+	ctx.fillRect(canvas.width - PLAYER_WIDTH, game.player2.y, PLAYER_WIDTH, PLAYER_HEIGHT);
 	// Draw ball
 	ctx.beginPath();
 	ctx.fillStyle = 'white';
 	ctx.arc(game.ball.x, game.ball.y, game.ball.r, 0, Math.PI * 2, false);
 	ctx.fill();
 }
-document.addEventListener
-(
-	'DOMContentLoaded', function ()
+
+function play() {
+    game.ball.x += game.ball.speed.x;
+    game.ball.y += game.ball.speed.y;
+    draw();
+    requestAnimationFrame(play);
+}
+
+document.addEventListener('DOMContentLoaded', function ()
 	{
 	    canvas = document.getElementById('canvas');
 		game =
@@ -49,9 +56,15 @@ document.addEventListener
 			{
 	        	x: canvas.width / 2,
 	            y: canvas.height / 2,
-	            r: 5
+	            r: 5,
+				speed:
+				{
+					x: 2,
+					y: 2
+				}
 	        }
 	    }
 	    draw();
+		play();
 	}
 );
