7587fabaef43fcff38485c52eb559fec36768717
Sylvain Ducamp <sylducam@gmail.com>
2022-12-26 18:28:12 +0100

    the game works

--- srcs/frontend/src/pong_the_one/pong.js
+++ srcs/frontend/src/pong_the_one/pong.js
@@ -3,28 +3,30 @@
 let canvas; // del need type
 let game; // del need type
 
-const PLAYER_HEIGHT = 100;
-const PLAYER_WIDTH = 5;
+const PLAYER_HEIGHT = 100
+const PLAYER_WIDTH = 5
+const BALL_SPEED = 8
 const UP = 38
 const DOWN = 40
+const STEP = 12
 const W = 87
 const S = 83
 
 function draw()
 {
-    let ctx = canvas.getContext('2d');
+	let ctx = canvas.getContext('2d');
 	
 	// Draw field
 	ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	// Draw net
 	ctx.lineWidth = 5;
 	ctx.strokeStyle = 'white';
-    ctx.beginPath();
+	ctx.beginPath();
 	ctx.setLineDash([5, 15]);
-    ctx.moveTo(canvas.width / 2, 0);
-    ctx.lineTo(canvas.width / 2, canvas.height);
-    ctx.stroke();
+	ctx.moveTo(canvas.width / 2, 0);
+	ctx.lineTo(canvas.width / 2, canvas.height);
+	ctx.stroke();
 	// Draw players
 	ctx.fillStyle = 'white';
 	ctx.fillRect(0, game.player1.y, PLAYER_WIDTH, PLAYER_HEIGHT);
@@ -36,33 +38,73 @@ function draw()
 	ctx.fill();
 }
 
-function play() {
-    game.ball.x += game.ball.speed.x;
-    game.ball.y += game.ball.speed.y;
-    draw();
-    requestAnimationFrame(play);
+function play()
+{
+	draw();
+	ballMove();
+	requestAnimationFrame(play);
 }
 
 function playerMove(event)
 {
-	if (event.keyCode === UP && game.player1.y >= 0)
+	if (event.keyCode === W && game.player1.y >= 0)
 	{
-		game.player1.y -= 7;
+		game.player1.y -= STEP;
 	}
-	if (event.keyCode === DOWN  && game.player1.y <= canvas.height - PLAYER_HEIGHT)
+	if (event.keyCode === S  && game.player1.y <= canvas.height - PLAYER_HEIGHT)
 	{
-		game.player1.y += 7;
+		game.player1.y += STEP;
 	}
-	if (event.keyCode === W && game.player2.y >= 0)
+	if (event.keyCode === UP && game.player2.y >= 0)
 	{
-		game.player2.y -= 7;
+		game.player2.y -= STEP;
 	}
-	if (event.keyCode === S  && game.player2.y <= canvas.height - PLAYER_HEIGHT)
+	if (event.keyCode === DOWN  && game.player2.y <= canvas.height - PLAYER_HEIGHT)
 	{
-		game.player2.y += 7;
+		game.player2.y += STEP;
 	}
 }
 
+function ballMove()
+{
+	// Rebounds on top and bottom
+	if (game.ball.y > canvas.height || game.ball.y < 0)
+	{
+		game.ball.speed.y *= -1;
+	}
+
+	// The ball reaches the left or right limit
+	if (game.ball.x > canvas.width - PLAYER_WIDTH)
+	{
+		collide(game.player2);
+	}
+	else if (game.ball.x < PLAYER_WIDTH)
+	{
+		collide(game.player1);
+	}
+
+	// The ball's speed increases each time
+	game.ball.x += game.ball.speed.x;
+	game.ball.y += game.ball.speed.y;
+}
+
+function collide(opponent) {
+	// The player misses the ball
+	if (game.ball.y < opponent.y || game.ball.y > opponent.y + PLAYER_HEIGHT) {
+		// Set ball and players to the center
+		game.ball.x = canvas.width / 2;
+		game.ball.y = canvas.height / 2;
+		game.player1.y = canvas.height / 2 - PLAYER_HEIGHT / 2;
+		game.player2.y = canvas.height / 2 - PLAYER_HEIGHT / 2;
+		
+		// Reset speed
+		game.ball.speed.x = BALL_SPEED;
+		game.ball.speed.y = BALL_SPEED;
+	} else {
+		// Increase speed and change direction
+		game.ball.speed.x *= -1.2;
+	}
+}
 
 document.addEventListener('DOMContentLoaded', function ()
 {
@@ -84,8 +126,8 @@ document.addEventListener('DOMContentLoaded', function ()
 			r: 5,
 			speed:
 			{
-				x: 2,
-				y: 2
+				x: BALL_SPEED,
+				y: BALL_SPEED
 			}
 		}
 	};
