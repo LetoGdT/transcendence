9fc3b097130df15dd1b8add71446f349c36110b9
Sylvain Ducamp <sylducam@gmail.com>
2023-01-03 19:02:22 +0100

    added some help from alexis

---
+++ srcs/frontend/src/pong_the_one/alexis.ts
@@ -0,0 +1,68 @@
+
+class Player {
+	public x : number;
+	public y : number;
+	public w : number;
+	public h : number;
+	
+	constructor(x: number, y: number, w: number, h: number) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+
+	render(ctx: CanvasRenderingContext2D) {
+		ctx.drawREctrangle(x, y ... )
+	}
+}
+
+class Ball {}
+
+class Game {
+	private player1 : Player;
+	private player2 : Player;
+	private ball : Ball;
+
+	constructor(canvas: HTMLCanvasElement) {
+		const playerHeight = 40;
+
+		this.player1 = new Player(
+			10,
+			(canvas.height - playerHeight) / 2,
+			10,
+			playerHeight			
+		);
+		this.player2 = new Player(
+			canvas.width - 10 - 10,
+			(canvas.height - playerHeight) / 2,
+			10,
+			playerHeight
+		);
+	}
+
+	render(ctx: CanvasRenderingContext2D) {
+		this.player1.render(ctx);
+		this.player2.render(ctx);
+		this.ball.render(ctx);
+	}
+
+	update() {
+		/* collisions */
+		
+	}
+}
+
+function start(canvas: HTMLCanvasElement) {
+	const game = new Game(canvas);
+
+	function doRender() {
+		game.render(canvas.getContext('2d') as CanvasRenderingContext2D);
+
+		window.requestAnimationFrame(doRender);
+	}
+
+	setInterval(() => game.update(), 20);
+
+	doRender();
+}
