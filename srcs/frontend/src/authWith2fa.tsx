65857d147c3beaa7026a5ffd561cd7d26bd74c4e
Lucille Burnet <lburnet@z2r6p6.42lyon.fr>
2023-01-04 14:49:05 +0100

    on essaie de recup le contenu d'un textfield

--- srcs/frontend/src/authWith2fa.tsx
+++ srcs/frontend/src/authWith2fa.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
-import {FormContainer, TextFieldElement} from 'react-hook-form-mui'
+// import {FormContainer, TextFieldElement} from 'react-hook-form-mui'
 
 const CodeOf2FATextField = styled(TextField)({
 	'& input:valid + fieldset': {
@@ -57,43 +57,43 @@ const SettingsButton = styled(Button)({
 	},
 });
 
-// export function AuthWith2FA(){
-// 	let Code2FA: string | undefined;	
+export function AuthWith2FA(){
+	let Code2FA: string | undefined;	
 
-// 	// const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
-// 	// 	console.log("handleInput");
-// 	// 	let newCode: string;
-// 	// 	newCode = e.target.value; 
-// 	// 	console.log(newCode);
-// 	// };
+	// const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
+	// 	console.log("handleInput");
+	// 	let newCode: string;
+	// 	newCode = e.target.value; 
+	// 	console.log(newCode);
+	// };
 
-// 	return(
-// 		<React.Fragment>
-// 			<h1>2FA</h1>
-// 			<CodeOf2FATextField
-// 				label="6 digits code"
-// 				InputLabelProps={{
-// 					sx:{
-// 						color:"white",
-// 					}
-// 				}}
-// 				variant="outlined"
-// 				// defaultValue="000000"
-// 				sx={{ input: {color: "grey"} }}
-// 				id="validation-outlined-input"
-// 				// onInput={(e: React.FormEvent<HTMLInputElement>) => handleInput(e)}
-// 				onSubmit={(e: React.SyntheticEvent) => {
-// 					e.preventDefault();
-// 					const target = e.target as typeof e.target & {
-// 					  code2FA: { value: string };
-// 					};
-// 					Code2FA = target.code2FA.value;
-// 				}}
-// 			/>
-// 			<p>Result : {Code2FA}</p>
-// 		</React.Fragment>
-// 	);
-// }
+	return(
+		<React.Fragment>
+			<h1>2FA</h1>
+			<CodeOf2FATextField
+				label="6 digits code"
+				InputLabelProps={{
+					sx:{
+						color:"white",
+					}
+				}}
+				variant="outlined"
+				// defaultValue="000000"
+				sx={{ input: {color: "grey"} }}
+				id="validation-outlined-input"
+				// onInput={(e: React.FormEvent<HTMLInputElement>) => handleInput(e)}
+				onChange={(e: React.SyntheticEvent) => {
+					e.preventDefault();
+					const target = e.target as typeof e.target & {
+					  Code2FA: { value: string };
+					};
+					Code2FA = target.Code2FA.value;
+				}}
+			/>
+			<p>Result : {Code2FA}</p>
+		</React.Fragment>
+	);
+}
 
 // export function AuthWith2FA(){
 // 	() => {
