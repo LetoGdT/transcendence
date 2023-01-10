a079d4a4827f35fdf983b7048043cf8c0f7cbafa
lucille <lucille.burnet@gmail.com>
2023-01-10 12:34:42 +0100

    y'a un return

--- srcs/frontend/src/Settings-zone.tsx
+++ srcs/frontend/src/Settings-zone.tsx
@@ -146,14 +146,14 @@ function Desactivate2FA(){
 	}, []);	
 }
 
-function ValidateButton(newAlias: string | undefined, newAvatar: string | undefined){
-	if (newAlias !== undefined){
-		SendNewAlias(newAlias);
-	}
-	if (newAvatar !== undefined){
-		SendNewAvatar(newAvatar);
-	}
-}
+// function ValidateButton(newAlias: string | undefined, newAvatar: string | undefined){
+// 	if (newAlias !== undefined){
+// 		SendNewAlias(newAlias);
+// 	}
+// 	if (newAvatar !== undefined){
+// 		SendNewAvatar(newAvatar);
+// 	}
+// }
 
 export function Settings(){
 	const [data, setResult] = useState<resultProps>();
