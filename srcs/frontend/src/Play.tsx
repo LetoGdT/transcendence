d4571fb80217f8f5e259572be1c847a21bcf1e15
lucille <lucille.burnet@gmail.com>
2023-01-13 13:52:59 +0100

    working on play

--- srcs/frontend/src/Play.tsx
+++ srcs/frontend/src/Play.tsx
@@ -1,11 +1,91 @@
+import './Play.css';
+import './App.css';
 import React from "react";
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
 import { PleaseConnect } from "./adaptable-zone";
+
+const NormalModeButton = styled(Button)({
+	boxShadow: 'none',
+	width: '400px',
+	height: '126px',
+	textTransform: 'none',
+	fontSize: 20,
+	padding: '6px 12px',
+	border: '1px solid',
+	lineHeight: 1.5,
+	backgroundColor: '#646464',
+	borderColor: '#646464',
+	fontFamily: [
+		'-apple-system',
+		'BlinkMacSystemFont',
+		'"Segoe UI"',
+		'Roboto',
+		'"Helvetica Neue"',
+		'Arial',
+		'sans-serif',
+		'"Apple Color Emoji"',
+		'"Segoe UI Emoji"',
+		'"Segoe UI Symbol"',
+	].join(','),
+	'&:hover': {
+		backgroundColor: '#007dd6',
+		borderColor: '#646464',
+		boxShadow: 'none',
+	},
+	'&:active': {
+		boxShadow: 'none',
+		backgroundColor: '#004d7b',
+		borderColor: '#646464',
+	},
+	'&:focus': {
+		adow: '0 0 0 0.2rem rgba(0,0,0,.5)',
+	},
+});
+
+const UnicornModeButton = styled(Button)({
+	boxShadow: 'none',
+	width: '400px',
+	height: '126px',
+	textTransform: 'none',
+	fontSize: 20,
+	padding: '6px 12px',
+	border: '1px solid',
+	lineHeight: 1.5,
+	backgroundColor: '#646464',
+	borderColor: '#646464',
+	color: '#34ebe5',
+	fontFamily: [
+		'"Comic Sans"'
+	].join(','),
+	'&:hover': {
+		backgroundColor: '#9542f5',
+		borderColor: '#646464',
+		boxShadow: 'none',
+	},
+	'&:active': {
+		boxShadow: 'none',
+		backgroundColor: '#7F1DEC',
+		borderColor: '#646464',
+	},
+	'&:focus': {
+		adow: '0 0 0 0.2rem rgba(0,0,0,.5)',
+	},
+});
 
 function Play(){
 	return(
 		<React.Fragment>
 			<h1>Play</h1>
-
+			<div className='Play-container'>
+				<div className='Play-button'>
+					<NormalModeButton variant="contained" disableRipple>PLAY<br></br>Classic Mode</NormalModeButton>
+				</div>
+				<div className='Play-button'>
+					<UnicornModeButton variant="contained" disableRipple>PLAY<br></br>Unicorn Mode</UnicornModeButton>
+				</div>
+			</div>
 		</React.Fragment>
 	);
 }
