99110abf8b1b413dc632d5e206b33e62d0143786
lburnet <lburnet@student.42lyon.fr>
2023-01-27 18:35:39 +0100

    working on manage channel

---
+++ srcs/frontend/src/ManageChannel.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import './SetChannel.css';
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+
+export function ManageChannel(){
+    let { cid } = React.useParam();
+
+    return();
+}
