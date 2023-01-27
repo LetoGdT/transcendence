3084e1184e7bf04936a2afa7d02ef012ef2e815c
lburnet <lburnet@student.42lyon.fr>
2023-01-27 22:42:40 +0100

    working on mana Channels

--- srcs/frontend/src/ManageChannel.tsx
+++ srcs/frontend/src/ManageChannel.tsx
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 
 import { NotFound } from './adaptable-zone';
+import { userStatus } from './tools';
 
 type userProps = {
 	id: number;
@@ -64,6 +65,80 @@ const ManageChannelButton = styled(Button)({
 	},
 });
 
+function SelectStatus(){
+
+	const handleClickSetPublic = async (event: React.MouseEvent<HTMLButtonElement>) => {
+		const response = await fetch('?',{//a changer
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'PATCH',
+			credentials: 'include',
+			// body: JSON.stringify({image_url: newPass})//a changer
+		});
+	}
+
+	const handleClickSetPrivate = async (event: React.MouseEvent<HTMLButtonElement>) => {
+		const response = await fetch('?',{//a changer
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'PATCH',
+			credentials: 'include',
+			// body: JSON.stringify({image_url: newPass})//a changer
+		});
+	}
+	
+	const handleClickSetProtected = async (event: React.MouseEvent<HTMLButtonElement>) => {
+		const response = await fetch('?',{//a changer
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'PATCH',
+			credentials: 'include',
+			// body: JSON.stringify({image_url: newPass})//a changer
+		});
+	}
+
+	if (channel.status === "public"){
+		return(
+			<React.Fragment>
+				<div className='Manage-Channel-button'>
+					<ManageChannelButton variant="contained" disableRipple onClick={handleClickSetPrivate}>Set private</ManageChannelButton><
+				</div>
+				<div className='Manage-Channel-button'>
+					<ManageChannelButton variant="contained" disableRipple onClick={handleClickSetProtected}>Set protected</ManageChannelButton><
+				</div>
+			</React.Fragment>
+		);
+	} else if (channel.status === "private"){
+		return(
+			<React.Fragment>
+				<div className='Manage-Channel-button'>
+					<ManageChannelButton variant="contained" disableRipple onClick={handleClickSetPublic}>Set public</ManageChannelButton><
+				</div>
+				<div className='Manage-Channel-button'>
+					<ManageChannelButton variant="contained" disableRipple onClick={handleClickSetProtected}>Set protected</ManageChannelButton><
+				</div>
+			</React.Fragment>
+		);
+	} else {
+		return(
+			<React.Fragment>
+				<div className='Manage-Channel-button'>
+					<ManageChannelButton variant="contained" disableRipple onClick={handleClickSetPublic}>Set public</ManageChannelButton><
+				</div>
+				<div className='Manage-Channel-button'>
+					<ManageChannelButton variant="contained" disableRipple onClick={handleClickSetPrivate}>Set private</ManageChannelButton><
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
 export function ManageChannel(){
 	let { cid } = React.useParam();
 	const [me, setMe] = React.useState<userProps>();
@@ -79,6 +154,8 @@ export function ManageChannel(){
 			const jsonData = await data.json();
 			setMe(jsonData);
 		};
+		
+		//recuperer les info du channel dont id est cid
 	
 		api();
 	}, []);
@@ -86,6 +163,18 @@ export function ManageChannel(){
 	const handleInputPwd = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 		setPassword(e.target.value);
 	};
+
+	const handleClickNewPwd = async (event: React.MouseEvent<HTMLButtonElement>) => {
+		const response = await fetch('?',{//a changer
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'PATCH',
+			credentials: 'include',
+			// body: JSON.stringify({image_url: newPass})//a changer
+		});
+	}
 
 	// if (/*me n'est pas admin*/){
 	// 	return(
@@ -124,13 +213,27 @@ export function ManageChannel(){
 								/>
 							</Box>
 						</div>
-						<div>
-						<ManageChannelButton variant="contained" disableRipple onClick={handleClickNew}>Save Password</ManageChannelButton><
+						<div className='Manage-Channel-button'>
+							<ManageChannelButton variant="contained" disableRipple onClick={handleClickNewPwd}>Save Password</ManageChannelButton><
 						</div>
 					</div>
-					<div>
-						
+					<div className='Manage-Channel-container-div'>
+						<div>
+							Change status: 
+						</div>
+						<div>
+							<SelectStatus />
+						</div>
 					</div>
+					<h4>List of users</h4>
+					{
+						//recuperer la liste des users
+						users.length > 0 && users?.map((user:any) => {
+							return(
+								<DisplayUser user={user} />
+							);
+						})
+					}
 				</div>
 			</React.Fragment>
 		);
