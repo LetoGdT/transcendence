6eee08af92b777fd32a60f3810c3f28e7a4f4ae1
lburnet <lburnet@student.42lyon.fr>
2023-01-27 18:22:38 +0100

    working on set channel

--- srcs/frontend/src/SetChannel.tsx
+++ srcs/frontend/src/SetChannel.tsx
@@ -1,9 +1,130 @@
 import './App.css';
+import './SetChannel.css';
 import React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+
+const SetChannelTextField = styled(TextField)({
+	'& input:valid + fieldset': {
+		borderColor: 'white',
+		borderWidth: 2,
+	},
+	'& input:invalid + fieldset': {
+		borderColor: 'red',
+		borderWidth: 2,
+	},
+	'& input:valid:focus + fieldset': {
+		borderLeftWidth: 6,
+		padding: '4px !important', // override inline-style
+	},
+});
+
+const SetChannelButton = styled(Button)({
+	boxShadow: 'none',
+	textTransform: 'none',
+	fontSize: 16,
+	padding: '6px 12px',
+	border: '1px solid',
+	lineHeight: 1.5,
+	backgroundColor: '#646464',
+	borderColor: '#646464',
+	fontFamily: [
+		'-apple-system',
+		'BlinkMacSystemFont',
+		'"Segoe UI"',
+		'Roboto',
+		'"Helvetica Neue"',
+		'Arial',
+		'sans-serif',
+		'"Apple Color Emoji"',
+		'"Segoe UI Emoji"',
+		'"Segoe UI Symbol"',
+	].join(','),
+	'&:hover': {
+		backgroundColor: '#3b9b3b',
+		borderColor: '#646464',
+		boxShadow: 'none',
+	},
+	'&:active': {
+		boxShadow: 'none',
+		backgroundColor: '#4a7a4a',
+		borderColor: '#646464',
+	},
+	'&:focus': {
+		xShadow: '0 0 0 0.2rem rgba(0,0,0,.5)',
+	},
+});
 
 export function SetChannel(){
-    return{
-		<React.Fragment>
-        </React.Fragment>
-    };
+	const [name, setName] = React.useState("");
+	const [password, setPassword] = React.useState("");
+
+	const handleInputName = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setName(e.target.value);
+	};
+
+	const handleInputPsw = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setPassword(e.target.value);
+	};
+
+	const handleClickSetChannel = async (event: React.MouseEvent<HTMLButtonElement>) => {
+		//?
+	};
+
+    return(
+		<div className='Set-Channel-container'>
+			<div className='Set-Channel-container-div'>
+				<div>
+					<Box
+						component="form"
+						noValidate
+						sx={{
+							display: 'grid',
+							gap: 2,
+						}}
+					>
+						<SetChannelTextField
+							label="Name"
+							InputLabelProps={{
+							sx:{
+								color:"white",
+							}
+							}}
+							variant="outlined"
+							defaultValue="*.jpg or *.png"
+							sx={{ input: { color: 'grey' } }}
+							id="validation-outlined-input"
+							onChange={handleInputName}
+						/>
+					</Box>
+				</div>
+				<div>
+					<Box
+						component="form"
+						noValidate
+						sx={{
+							display: 'grid',
+							gap: 2,
+						}}
+					>
+						<SetChannelTextField
+							label="Password"
+							InputLabelProps={{
+							sx:{
+								color:"white",
+							}
+							}}
+							variant="outlined"
+							defaultValue="*.jpg or *.png"
+							sx={{ input: { color: 'grey' } }}
+							id="validation-outlined-input"
+							onChange={handleInputPsw}
+						/>
+					</Box>
+				</div>
+			</div>
+			<div><SetChannelButton variant="contained" disableRipple onClick={handleClickSetChannel}>Create Channel</SetChannelButton></div>
+		</div>
+    );
 }
