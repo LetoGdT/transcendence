55a51b555866880f30b9ece476aad064544ed591
Sylvain Ducamp <sylducam@gmail.com>
2023-01-28 14:15:59 +0100

    Fix minor bugs (paddle y position stuck at the top and error when joining invalid spectator game)

--- srcs/backend/src/websocket/websocket.gateway.ts
+++ srcs/backend/src/websocket/websocket.gateway.ts
@@ -34,7 +34,7 @@ class RemotePlayer {
 		this.socket = socket;
 		this.user = user;
 		this.score = 0;
-		this.y = 0;
+		this.y = GAME_HEIGHT / 2;
 	}
 
 	emit(ev: string, ...args: any[]) {
@@ -422,6 +422,10 @@ class GameManager {
 		this.games.splice(toRemove, 1);
 	}
 
+	getGameById(id: number): Game | undefined {
+		return this.games.find(e => e.id === id);
+	}
+
 	removeFinishedGames() {
 		let i, j;
 		
@@ -605,13 +609,13 @@ export class MySocketGateway implements OnGatewayConnection,
 		const index = this.clients.findIndex(connection => connection.client.id == client.id);
 
 		if (index >= 0) {
-			for (const game of gameManager.getGames())
-			{
-				if (game.id === body.game_id) {
-					game.addSpectator(client);
-					break;
-				}
+			const game = gameManager.getGameById(body.game_id);
+			
+			if (null == game) {
+				throw new WsException("The game doesn't exist");
 			}
+
+			game.addSpectator(this.clients[index].client);
 		}
 	}
 
